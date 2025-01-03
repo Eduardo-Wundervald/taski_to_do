78d343f939ed9816bc3b3f2f7b08739a267fc437
LucasPetruci <lucaspetruci100@gmail.com>
2025-01-03 16:41:32 -0300

    refactor: rename HomeView to HomeScreen and update widget structure for improved readability

--- lib/src/presentation/view/home_screen_view.dart
+++ lib/src/presentation/view/home_screen_view.dart
@@ -1,21 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:taski_to_do/core/widgets/bottom_nav_bar.dart';
 import 'package:provider/provider.dart';
-import '../viewmodel/home_view_model.dart';
+import 'package:taski_to_do/src/presentation/viewmodel/home_view_model.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+import '../../../core/widgets/bottom_nav_bar.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     final viewModel = context.watch<HomeViewModel>();
+
     return Scaffold(
-      body: const Center(
-        child: Text('Todo View'),
-      ),
+      body: viewModel.currentScreen,
       bottomNavigationBar: MyBottomNavBar(
-        onTabChange: viewModel.onTabChange,
         currentIndex: viewModel.currentIndex,
+        onTabChange: viewModel.onTabChange,
       ),
     );
   }
