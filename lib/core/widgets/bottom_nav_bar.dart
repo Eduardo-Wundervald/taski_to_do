70bab1af6a30d9e43c31d368b4b54cb04dcb9b16
LucasPetruci <lucaspetruci100@gmail.com>
2025-01-03 16:41:19 -0300

    refactor: update bottom navigation bar with custom implementation and improved tab handling

--- lib/core/widgets/bottom_nav_bar.dart
+++ lib/core/widgets/bottom_nav_bar.dart
@@ -1,35 +1,86 @@
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 
 class MyBottomNavBar extends StatelessWidget {
   final int currentIndex;
   final void Function(int)? onTabChange;
 
-  const MyBottomNavBar(
-      {super.key, required this.onTabChange, required this.currentIndex});
+  const MyBottomNavBar({
+    super.key,
+    required this.onTabChange,
+    required this.currentIndex,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(vertical: 20),
-      child: GNav(
-        selectedIndex: currentIndex,
-        color: Colors.grey[400],
-        activeColor: Colors.grey.shade700,
-        tabActiveBorder: Border.all(color: Colors.white),
-        tabBackgroundColor: Colors.grey.shade100,
-        mainAxisAlignment: MainAxisAlignment.center,
-        tabBorderRadius: 16,
-        gap: 8,
-        onTabChange: (value) => onTabChange!(value),
-        tabs: const [
-          GButton(
-            icon: Icons.home,
-            text: 'Shop',
+      color: Colors.white,
+      padding: const EdgeInsets.symmetric(vertical: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildNavItem(
+            icon: Icons.format_list_bulleted,
+            label: "Todo",
+            index: 0,
+            currentIndex: currentIndex,
+            onTap: onTabChange,
           ),
-          GButton(
-            icon: Icons.shopping_bag_rounded,
-            text: 'Carrinho',
+          _buildNavItem(
+            icon: Icons.add_circle_outline,
+            label: "Criar",
+            index: 1,
+            currentIndex: currentIndex,
+            onTap: onTabChange,
+          ),
+          _buildNavItem(
+            icon: Icons.search,
+            label: "Search",
+            index: 2,
+            currentIndex: currentIndex,
+            onTap: onTabChange,
+          ),
+          _buildNavItem(
+            icon: Icons.check_circle_outline,
+            label: "Done",
+            index: 3,
+            currentIndex: currentIndex,
+            onTap: onTabChange,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNavItem({
+    required IconData icon,
+    required String label,
+    required int index,
+    required int currentIndex,
+    required void Function(int)? onTap,
+  }) {
+    final isSelected = index == currentIndex;
+
+    return GestureDetector(
+      onTap: () => onTap?.call(index),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            color: isSelected
+                ? const Color.fromRGBO(0, 172, 255, 1)
+                : const Color.fromRGBO(198, 207, 220, 1),
+            size: 28,
+          ),
+          const SizedBox(height: 4),
+          Text(
+            label,
+            style: TextStyle(
+              color: isSelected
+                  ? const Color.fromRGBO(0, 172, 255, 1)
+                  : const Color.fromRGBO(198, 207, 220, 1),
+              fontSize: 12,
+            ),
           ),
         ],
       ),
