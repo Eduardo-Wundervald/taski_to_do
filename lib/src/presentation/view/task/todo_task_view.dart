8b715866268b8af5027fbc7b6491b081703c1d46
LucasPetruci <lucaspetruci100@gmail.com>
2025-01-04 00:32:55 -0300

    feat: enhance TodoTask view with Logo and Profile widgets in app bar

--- lib/src/presentation/view/task/todo_task_view.dart
+++ lib/src/presentation/view/task/todo_task_view.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/widgets/logo.dart';
+import '../../../../core/widgets/profile.dart';
+
 class TodoTask extends StatelessWidget {
   const TodoTask({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Todo Task"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Row(
+          children: [
+            Logo(),
+            Spacer(),
+            Profile(name: "John"),
+          ],
+        ),
       ),
+      body: Text("Todo Task"),
     );
   }
 }
