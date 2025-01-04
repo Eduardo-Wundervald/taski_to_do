5c6a118ef3682928ba3d2a69f951fb310fbcdd27
LucasPetruci <lucaspetruci100@gmail.com>
2025-01-04 00:32:32 -0300

    feat: add Profile widget for displaying user information

---
+++ lib/core/widgets/profile.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  final String name;
+  const Profile({super.key, required this.name});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          name,
+          style: const TextStyle(
+            color: Color.fromRGBO(63, 61, 86, 1),
+            fontFamily: "Urbanist",
+            fontSize: 18,
+          ),
+        ),
+        const SizedBox(width: 8),
+        ClipOval(
+          child: Image.asset(
+            "assets/images/profile_pic.jpg",
+            width: 42,
+            height: 42,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ],
+    );
+  }
+}
