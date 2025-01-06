ab20f07ab728d94ca1131716902a82f14e952c7e
LucasPetruci <lucaspetruci100@gmail.com>
2025-01-06 18:24:10 -0300

    feat: implement Hive support for Task model with serialization

--- lib/src/data/model/task.dart
+++ lib/src/data/model/task.dart
@@ -1,7 +1,16 @@
-class Task {
+import 'package:hive/hive.dart';
+part 'task.g.dart';
+
+@HiveType(typeId: 0)
+class Task extends HiveObject {
+  @HiveField(0)
   final String title;
+
+  @HiveField(1)
   final String description;
-  final bool isDone;
+
+  @HiveField(2)
+  bool isDone;
 
   Task({
     required this.title,
