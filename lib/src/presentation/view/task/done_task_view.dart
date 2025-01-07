6bededdc8cc472a8b4016855af44b369da629edf
LucasPetruci <lucaspetruci100@gmail.com>
2025-01-06 21:13:29 -0300

    feat: enhance DoneTask view with completed tasks display and delete functionality

--- lib/src/presentation/view/task/done_task_view.dart
+++ lib/src/presentation/view/task/done_task_view.dart
@@ -1,13 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../../../core/widgets/logo.dart';
+import '../../../../core/widgets/profile.dart';
+import '../../../../core/widgets/view_task.dart';
+import '../../viewmodel/done_task_view_model.dart';
 
 class DoneTask extends StatelessWidget {
   const DoneTask({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Delete Task"),
+    final doneTaskViewModel = context.watch<DoneTaskViewModel>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Padding(
+          padding: EdgeInsets.only(top: 20, left: 8, right: 8),
+          child: Row(
+            children: [
+              Logo(),
+              Spacer(),
+              Profile(name: "John"),
+            ],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 26, right: 26, top: 30),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                const Text(
+                  "Completed Tasks",
+                  style: TextStyle(
+                    color: Color.fromRGBO(63, 61, 86, 1),
+                    fontFamily: "Urbanist",
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20,
+                  ),
+                ),
+                const Spacer(),
+
+                // Clear All
+                TextButton(
+                  onPressed: () => doneTaskViewModel.deleteAllTaskDone(),
+                  child: const Text(
+                    "Delete All",
+                    style: TextStyle(
+                      color: Color.fromRGBO(255, 94, 94, 1),
+                      fontFamily: "Urbanist",
+                      fontWeight: FontWeight.w700,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            doneTaskViewModel.completedTasks.isEmpty
+                ? const Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 20),
+                      child: Text(
+                        "No tasks completed yet.",
+                        style: TextStyle(
+                          color: Color.fromRGBO(141, 156, 184, 1),
+                          fontFamily: "Urbanist",
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  )
+                : Expanded(
+                    child: ListView.builder(
+                      itemCount: doneTaskViewModel.completedTasks.length,
+                      itemBuilder: (context, index) {
+                        final undoneTask = doneTaskViewModel.completedTasks;
+                        return ViewTask(
+                          title: undoneTask[index].title,
+                          description: undoneTask[index].description,
+                          isDone: undoneTask[index].isDone,
+                          onTaskDone: (value) {
+                            // doneTaskViewModel.updateTask(
+                            //   undoneTask[index].copyWith(isDone: value),
+                            // );
+                          },
+                          onDelete: () {
+                            doneTaskViewModel.deleteTask(index);
+                          },
+                        );
+                      },
+                    ),
+                  ),
+          ],
+        ),
       ),
     );
   }
